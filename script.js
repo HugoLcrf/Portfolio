8781f4a7d43006072ab594e7f48fd6d46c9ed1a1
HugoLcrf <lecerf.hugo04@gmail.com>
2023-11-13 15:21:03 +0100

    first push

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
+document.querySelector('button').addEventListener('click', toggleMenu);
