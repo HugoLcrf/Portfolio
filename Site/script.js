b422a09d7442bb2d402556e64eac5dcf3b9e4965
HugoLcrf <lecerf.hugo04@gmail.com>
2023-11-13 17:39:11 +0100

    NAVBAR

--- Site/script.js
+++ Site/script.js
@@ -1,10 +1,41 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-}
 
-document.querySelector('button').addEventListener('click', toggleMenu);
+var rotation = 0;
+var targetRotation = 0;
+var rotationStep = 2; // La vitesse de rotation
+
+var rotate = function(element, degree) {
+  var bbox = element.getBBox();
+  var centerX = bbox.x + bbox.width / 2;
+  var centerY = bbox.y + bbox.height / 2;
+  var transformValue = "rotate(" + degree + " " + centerX + " " + centerY + ")";
+  element.setAttribute("transform", transformValue);
+};
+
+var animateRotation = function() {
+  if (rotation !== targetRotation) {
+    if (Math.abs(rotation - targetRotation) <= rotationStep) {
+      rotation = targetRotation;
+    } else if (rotation < targetRotation) {
+      rotation += rotationStep;
+    }
+    rotate(document.getElementById("losange"), rotation);
+    requestAnimationFrame(animateRotation);
+  }
+};
+
+var myButton = document.getElementById("buttonNav");
+var menu = document.getElementById("menu");
+
+myButton.addEventListener("click", function() {
+  targetRotation += 135; // Rotation cible à 60 degrés de plus
+  animateRotation();
+
+  // Afficher ou masquer le menu
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+});
+
+
