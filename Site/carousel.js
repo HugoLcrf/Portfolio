e85f1839d533d0ada7b2eadf15d217a6543eee11
HugoLcrf <lecerf.hugo04@gmail.com>
2023-11-26 17:57:58 +0100

    Some news

--- Site/carousel.js
+++ Site/carousel.js
@@ -1,38 +1,38 @@
-document.getElementById("carousel").addEventListener('wheel', function(event) {
-  var modifier = 1;
+// document.getElementById("carousel").addEventListener('wheel', function(event) {
+//   var modifier = 1;
+  
 
-  if (event.deltaMode == event.DOM_DELTA_PIXEL) {
-    modifier = 1;
-  } else if (event.deltaMode == event.DOM_DELTA_LINE) {
-    modifier = parseInt(getComputedStyle(this).lineHeight);
-  } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
-    modifier = this.clientHeight;
-  }
+//   if (event.deltaMode == event.DOM_DELTA_PIXEL) {
+//     modifier = 1;
+//   } else if (event.deltaMode == event.DOM_DELTA_LINE) {
+//     modifier = parseInt(getComputedStyle(this).lineHeight);
+//   } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
+//     modifier = this.clientHeight;
+//   }
 
-  if (isNaN(modifier)) {
-    modifier = 10;
-  }
+//   if (isNaN(modifier)) {
+//     modifier = 10;
+//   }
 
-  if (event.deltaY != 0) {
-    this.scrollLeft += modifier * event.deltaY;
-    event.preventDefault();
+//   if (event.deltaY != 0) {
+//     this.scrollLeft += modifier * event.deltaY;
+//     event.preventDefault();
 
-    // Vérifier si le défilement horizontal atteint les limites
-    if (this.scrollLeft + this.clientWidth >= this.scrollWidth) {
-      // Si oui, passer à la section suivante
-      document.getElementById("Bottom").scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-});
+//     // Vérifier si le défilement horizontal atteint les limites
+//     if (this.scrollLeft + this.clientWidth >= this.scrollWidth) {
+//       // Si oui, passer à la section suivante
+//       document.getElementById("Bottom").scrollIntoView({ behavior: 'smooth' });
+//     }
+//   }
+// });
 
-function handleHorizontalScroll(event) {
-  event.preventDefault();
-}
+// function handleHorizontalScroll(event) {
+//   event.preventDefault();
+// }
 
-function handleVerticalScroll(event) {
-  document.getElementById("Bottom").scrollIntoView({ behavior: 'smooth' });
-}
-
+// function handleVerticalScroll(event) {
+//   document.getElementById("Bottom").scrollIntoView({ behavior: 'smooth' });
+// }
 
 //  Contenue a droite
 
